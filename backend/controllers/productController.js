0d95b8f5e72729c9c0b913a2b2b2a84401e98502
resetmerlin <resetmerlin@gmail.com>
2023-06-24 21:34:28 +0900

    feat: 특정 product ID get controller 추가

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,4 +10,22 @@ const getProducts = asyncHandler(async (req, res) => {
 
   res.json(cyberProducts);
 });
-export { getProducts };
+
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const cyberProduct = await cyberProductModel.findById(req.params.id);
+
+  if (cyberProduct) {
+    return res.json(cyberProduct);
+  } else {
+    res.status(404);
+    //custom error handler
+    throw new Error("Product not found");
+  }
+
+  res.json(cyberProduct);
+});
+
+export { getProductById, getProducts };
