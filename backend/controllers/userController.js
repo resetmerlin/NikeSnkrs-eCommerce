8173347d011fa306c526264ec20aa845563014c8
resetmerlin <resetmerlin@gmail.com>
2023-06-28 21:36:26 +0900

    feat: 유저 정보 get 함수 추가

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,4 +22,24 @@ const authenticationUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email of password");
   }
 });
-export { authenticationUser };
+
+///@desc GET user profile
+// @route GET/api/user/profile
+// @access  Private
+const getClientProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authenticationUser, getClientProfile };
