e5c486a442da4e8bc0e8c2eccffd7ddb93d0860c
resetmerlin <resetmerlin@gmail.com>
2023-06-28 21:38:26 +0900

    feat: 유저 POST 회원가입 함수 추가

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,6 +22,39 @@ const authenticationUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email of password");
   }
 });
+// @desc Register a new user
+// @route POST/api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExistence = await User.findOne({ email });
+
+  if (userExistence) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    //we will gonna crypt it
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: createToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 ///@desc GET user profile
 // @route GET/api/user/profile
@@ -68,4 +101,9 @@ const updateClientProfile = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 });
-export { authenticationUser, getClientProfile, updateClientProfile };
+export {
+  authenticationUser,
+  getClientProfile,
+  updateClientProfile,
+  registerUser,
+};
