c3ede9cf72af76102500343e09d528227298ec99
resetmerlin <resetmerlin@gmail.com>
2023-06-29 21:46:34 +0900

    feat: 주문 정보 결제 상태로 변경 PUT controller 함수 추가

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -58,4 +58,26 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @desc Update order to paid
+// @route PUT/api/orders:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+    };
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not Found");
+  }
+});
+export { addOrderItems, getOrderById, updateOrderToPaid };
