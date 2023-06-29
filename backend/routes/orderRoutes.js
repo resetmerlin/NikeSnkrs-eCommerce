628034c01297912e720edb5df6c9689413eef436
resetmerlin <resetmerlin@gmail.com>
2023-06-29 21:51:27 +0900

    feat: 아이디값에 따른 주문 get route 추가

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 
 router.route("/").post(addOrderItems);
+router.route("/:id").get(getOrderById);
