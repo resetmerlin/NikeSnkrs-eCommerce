dfa4004bee56ed965bcd34a3fd633ce62d5cf1d8
resetmerlin <resetmerlin@gmail.com>
2023-06-30 11:13:39 +0900

    feat: order api route에 protection jwt 추가

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 const router = express.Router();
+import { protection } from "../middleware/authMiddleware.js";
+
 import {
   addOrderItems,
   getOrderById,
@@ -7,9 +9,9 @@ import {
   getMyOrders,
 } from "../controllers/orderController.js";
 
-router.route("/").post(addOrderItems);
-router.route("/:id").get(getOrderById);
-router.route("/:id/pay").put(updateOrderToPaid);
-router.route("/myorders").get(getMyOrders);
+router.route("/").post(protection, addOrderItems);
+router.route("/:id").get(protection, getOrderById);
+router.route("/:id/pay").put(protection, updateOrderToPaid);
+router.route("/myorders").get(protection, getMyOrders);
 
 export default router;
