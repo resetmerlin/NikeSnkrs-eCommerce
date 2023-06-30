6b7fc08dabd31bc08c020a72270cf77b2f2a5219
resetmerlin <resetmerlin@gmail.com>
2023-06-30 13:09:51 +0900

    feat: error Handler middleware 추가

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -4,4 +4,14 @@ const notFound = (req, res, next) => {
   next(error);
 };
 
-export { notFound };
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+  next();
+};
+
+export { notFound, errorHandler };
