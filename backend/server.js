2c6036724a87c184b561234454045af827097fd8
resetmerlin <resetmerlin@gmail.com>
2023-06-30 11:16:55 +0900

    feat: paypal client ID get API  추가

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
+
 const PORT = process.env.PORT;
 
 //listen() method creates a listener on the specified port or path.
