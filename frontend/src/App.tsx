ffe18654dc60578d240d6bcc6bbda76ccfd3fd6c
resetmerlin <resetmerlin@gmail.com>
2023-10-14 18:29:10 +0900

    feat: Put routes on RouterProvider in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router';
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
