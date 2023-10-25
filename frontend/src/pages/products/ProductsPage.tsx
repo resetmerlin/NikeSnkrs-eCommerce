4aba250d7825d078d2bffcf639b3d67cf62c5ebe
resetmerlin <resetmerlin@gmail.com>
2023-10-25 12:04:10 +0900

    feat: Add templates  component with fetch products hook

--- frontend/src/pages/products/ProductsPage.tsx
+++ frontend/src/pages/products/ProductsPage.tsx
@@ -1,20 +1,30 @@
+import { useEffect } from 'react';
 import { AtomicTitle } from '../../components/atoms';
 import { Layout } from '../../components/layouts';
 import { ItemLists } from '../../components/organisms';
-import { ChildCenter, ChildTopLeft } from '../../components/template/Child';
-import Parent from '../../components/template/Parent';
+import { Template } from '../../components/template';
+import { useGetProductsQuery } from '../../features/api/apiSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { productsAdded } from '../../features/product/productReducers';
 
 export default function ProductsPage() {
+  const dispatch = useAppDispatch();
+  const { data } = useGetProductsQuery();
+  const selectProducts = useAppSelector((state) => state.products);
+
+  useEffect(() => {
+    if (data) dispatch(productsAdded(data));
+  }, [data]);
   return (
     <Layout>
-      <Parent>
-        <ChildTopLeft>
+      <Template.Default>
+        <Template.TopLeft>
           <AtomicTitle size="xs">Latest Products</AtomicTitle>
-        </ChildTopLeft>
-        <ChildCenter>
-          <ItemLists />
-        </ChildCenter>
-      </Parent>
+        </Template.TopLeft>
+        <Template.BottomCenter>
+          <ItemLists products={selectProducts[0]} />
+        </Template.BottomCenter>
+      </Template.Default>
     </Layout>
   );
 }
