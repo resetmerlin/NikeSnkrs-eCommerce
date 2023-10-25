80c243eee16d2e96eb80ec80866c5efe737af31a
resetmerlin <resetmerlin@gmail.com>
2023-10-25 12:01:21 +0900

    feat: Add Template object and add templates component on property of it

---
+++ frontend/src/components/template/template.ts
@@ -0,0 +1,13 @@
+import BottomCenterTemplate from './BottomCenterTemplate';
+import BottomRightTemplate from './BottomRightTemplate';
+import CenterTemplate from './CenterTemplate';
+import DefaultTemplate from './DefaultTemplate';
+import TopLeftTemplate from './TopLeftTemplate';
+
+export const Template = {
+  BottomCenter: BottomCenterTemplate,
+  BottomRight: BottomRightTemplate,
+  Center: CenterTemplate,
+  Default: DefaultTemplate,
+  TopLeft: TopLeftTemplate,
+};
