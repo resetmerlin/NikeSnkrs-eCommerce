caa4f6e97abcbcb3e5791ca2ab1a59a650dca0af
resetmerlin <resetmerlin@gmail.com>
2023-10-25 11:55:06 +0900

    refactor: Revise Parent component to DefaultTemplate template component
    The role of template component is to layout the component rather than put paint on it. To keep this interests and enable role of template, I used grid css.

--- frontend/src/components/template/Parent.tsx
+++
@@ -1,8 +0,0 @@
-import { TNormalElementProps } from '../../types';
-import './style.scss';
-
-interface IProps extends TNormalElementProps<HTMLDivElement> {}
-
-export default function Parent({ children }: IProps) {
-  return <div className="template__parent">{children}</div>;
-}
