37b488a70cc3ffc2a7eb56a235e01e4673a9596c
resetmerlin <resetmerlin@gmail.com>
2023-10-25 11:41:29 +0900

    feat: Add Product Slice Reducer

---
+++ frontend/src/features/product/productReducers.ts
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const productsSlice = createSlice({
+  name: 'products',
+  initialState: [],
+  reducers: {
+    productsAdded(state, action) {
+      const data = action.payload;
+      state.push(data);
+    },
+  },
+});
+
+export const { productsAdded } = productsSlice.actions;
+
+export default productsSlice.reducer;
