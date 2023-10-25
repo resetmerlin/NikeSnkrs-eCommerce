95f19d1f8b17bd35adf7991af4082e824c847ebd
resetmerlin <resetmerlin@gmail.com>
2023-10-25 11:52:59 +0900

    feat: Add map method to map products props to create Items

--- frontend/src/components/organisms/itemLists/ItemLists.tsx
+++ frontend/src/components/organisms/itemLists/ItemLists.tsx
@@ -1,33 +1,16 @@
 import { Link } from 'react-router-dom';
 import { Item } from '../../molecules';
 import './ItemLists.scss';
-export default function ItemLists() {
+export default function ItemLists({ products }) {
   return (
     <div className="item-lists">
-      <Link to="/product">
-        <Item />
-      </Link>
-      <Link to="/product">
-        <Item />
-      </Link>
-      <Link to="/product">
-        <Item />
-      </Link>
-      <Link to="/product">
-        <Item />
-      </Link>
-      <Link to="/product">
-        <Item />
-      </Link>
-      <Link to="/product">
-        <Item />
-      </Link>
-      <Link to="/product">
-        <Item />
-      </Link>
-      <Link to="/product">
-        <Item />
-      </Link>
+      {products?.map((product) => {
+        return (
+          <Link to={`/product/${product._id}`} key={product._id}>
+            <Item product={product} />
+          </Link>
+        );
+      })}
     </div>
   );
 }
