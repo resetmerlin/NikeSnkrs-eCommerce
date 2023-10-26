eeac6fde12561421dbdc73612fc12f46c5699557
resetmerlin <resetmerlin@gmail.com>
2023-10-26 15:48:39 +0900

    feat: Add size property in AtomicItem component

--- frontend/src/components/atoms/item/AtomicItem.tsx
+++ frontend/src/components/atoms/item/AtomicItem.tsx
@@ -1,9 +1,20 @@
 import { TNormalElementProps } from '../../../types';
 import './AtomicItem.scss';
 
+interface IProps extends TNormalElementProps<HTMLDivElement> {
+  size: 'xs' | 's' | 'm';
+}
+
 export default function AtomicItem({
   children,
   className = '',
-}: TNormalElementProps<HTMLDivElement>) {
-  return <div className={`${className}  item`}>{children}</div>;
+  color,
+  size,
+  ...props
+}: IProps) {
+  return (
+    <div {...props} className={`${className}  item item-${size}`}>
+      {children}
+    </div>
+  );
 }
