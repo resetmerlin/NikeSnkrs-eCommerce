31c51e6dc29d603dab6a449d6dd47a6adbd6d2f1
resetmerlin <resetmerlin@gmail.com>
2023-10-26 16:34:12 +0900

    refactor: Delete protextion on /login route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,19 +1,19 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
-import { protection } from "../middleware/authMiddleware.js";
+import { protection } from '../middleware/authMiddleware.js';
 
 import {
   authenticationUser,
   getClientProfile,
   registerUser,
   updateClientProfile,
-} from "../controllers/userController.js";
+} from '../controllers/userController.js';
 
-router.route("/login").post(protection, authenticationUser);
+router.route('/login').post(authenticationUser);
 router
-  .route("/profile")
+  .route('/profile')
   .get(protection, getClientProfile)
   .put(protection, updateClientProfile);
-router.route("/register").post(registerUser);
+router.route('/register').post(registerUser);
 
 export default router;
