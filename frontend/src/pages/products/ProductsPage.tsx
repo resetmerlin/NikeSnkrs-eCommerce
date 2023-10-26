d79ab3499ecee59260bb44f6a9e7157a52b310c7
resetmerlin <resetmerlin@gmail.com>
2023-10-26 16:14:55 +0900

    Refactor: Change Template component to Parent or Child template component

--- frontend/src/pages/products/ProductsPage.tsx
+++ frontend/src/pages/products/ProductsPage.tsx
@@ -1,30 +1,25 @@
-import { useEffect } from 'react';
-import { AtomicTitle } from '../../components/atoms';
+import {
+  AtomicTitle,
+  ChildTemplate,
+  ParentTemplate,
+} from '../../components/atoms';
 import { Layout } from '../../components/layouts';
 import { ItemLists } from '../../components/organisms';
-import { Template } from '../../components/template';
 import { useGetProductsQuery } from '../../features/api/apiSlice';
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
-import { productsAdded } from '../../features/product/productReducers';
 
 export default function ProductsPage() {
-  const dispatch = useAppDispatch();
   const { data } = useGetProductsQuery();
-  const selectProducts = useAppSelector((state) => state.products);
 
-  useEffect(() => {
-    if (data) dispatch(productsAdded(data));
-  }, [data]);
   return (
     <Layout>
-      <Template.Default>
-        <Template.TopLeft>
+      <ParentTemplate size="s">
+        <ChildTemplate position="topLeft">
           <AtomicTitle size="xs">Latest Products</AtomicTitle>
-        </Template.TopLeft>
-        <Template.BottomCenter>
-          <ItemLists products={selectProducts[0]} />
-        </Template.BottomCenter>
-      </Template.Default>
+        </ChildTemplate>
+        <ChildTemplate position="bottomCenter">
+          <ItemLists products={data} />
+        </ChildTemplate>
+      </ParentTemplate>
     </Layout>
   );
 }
