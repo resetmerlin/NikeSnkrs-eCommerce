4338a2c4330dba6106994225fb840071cf77a25b
resetmerlin <resetmerlin@gmail.com>
2023-10-26 16:16:48 +0900

    chore: Re export modules

--- frontend/src/components/atoms/index.ts
+++ frontend/src/components/atoms/index.ts
@@ -4,3 +4,5 @@ export * from './title';
 export * from './button';
 export * from './logo';
 export * from './item';
+export * from './icons';
+export * from './template';
--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -1,4 +1,8 @@
 export * from './card';
 export * from './item';
+export * from './itemInfo';
+export * from './itemColumn';
+export * from './button';
+
 // export * from './button';
 // export * from './intro';
