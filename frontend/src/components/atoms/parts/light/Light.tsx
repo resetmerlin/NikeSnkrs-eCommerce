b849f10897e36571672fda51d24bf6bf1da6e154
resetmerlin <resetmerlin@gmail.com>
2023-10-27 10:42:59 +0900

    feat: Add 3d light parts in Atom

---
+++ frontend/src/components/atoms/parts/light/Light.tsx
@@ -0,0 +1,19 @@
+export default function Light({ ...props }) {
+  return (
+    <>
+      <ambientLight intensity={1} />
+      <directionalLight
+        {...props}
+        position={[-5, 1, 1.3]}
+        intensity={5}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+      />
+    </>
+  );
+}
