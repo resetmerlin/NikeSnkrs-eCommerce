1d7b652e8f3fc4126768e541427c6445eef2c4e9
resetmerlin <resetmerlin@gmail.com>
2023-10-27 16:44:19 +0900

    chore: Export object again

--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -3,6 +3,7 @@ export * from './item';
 export * from './itemInfo';
 export * from './itemColumn';
 export * from './button';
+export * from './object';
 
 // export * from './button';
 // export * from './intro';
