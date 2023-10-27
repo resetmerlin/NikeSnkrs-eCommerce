57663bbd169d9cfe25f0e09a671f3ff8e853be4e
resetmerlin <resetmerlin@gmail.com>
2023-10-28 07:45:28 +0900

    feat: Add suspense on Canvas

--- frontend/src/components/molecules/object/Object.tsx
+++ frontend/src/components/molecules/object/Object.tsx
@@ -1,15 +1,18 @@
+import { Suspense } from 'react';
 import { Canvas, Floor, Fog, Light, Model } from '../../atoms';
 import { OrbitControls } from '@react-three/drei';
 
 export default function Object({ model }) {
   return (
     <Canvas>
-      <Light />
-      <Fog args={['black', 30]} />
-      <Model model={model} scale={6} />
+      <Suspense fallback={null}>
+        <Light />
+        <Fog args={['black', 30]} />
+        <Model model={model} />
 
-      <Floor />
-      <OrbitControls />
+        <Floor />
+        <OrbitControls />
+      </Suspense>
     </Canvas>
   );
 }
