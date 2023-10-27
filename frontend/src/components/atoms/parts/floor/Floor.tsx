b4ac989d72c71bfe973539ee639285c1c8f6855e
resetmerlin <resetmerlin@gmail.com>
2023-10-27 10:43:37 +0900

    feat: Add 3d floor parts in Atom

---
+++ frontend/src/components/atoms/parts/floor/Floor.tsx
@@ -0,0 +1,8 @@
+export default function Floor() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]} receiveShadow>
+      <planeGeometry attach="geometry" args={[100, 100]} />
+      <shadowMaterial attach="material" opacity={0.5} />
+    </mesh>
+  );
+}
