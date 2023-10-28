2bf44a15983741ee1f25fce5358b03e6e34d0b85
resetmerlin <resetmerlin@gmail.com>
2023-10-29 08:53:44 +0900

    feat: Add userName options and type name in Atomic Input

--- frontend/src/components/atoms/input/AtomicInput.tsx
+++ frontend/src/components/atoms/input/AtomicInput.tsx
@@ -2,8 +2,8 @@ import { TNormalElementProps } from '../../../types';
 import './AtomicInput.scss';
 
 interface IProps extends TNormalElementProps<HTMLInputElement> {
-  type: 'password' | 'email';
-  name?: 'userPassword' | 'userEmail';
+  type: 'password' | 'email' | 'name';
+  name?: 'userPassword' | 'userEmail' | 'userName';
 }
 
 export default function AtomicInput({
