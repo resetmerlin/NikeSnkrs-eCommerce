6ed9a30a756c22b534ebc233895fb91a05234da9
resetmerlin <resetmerlin@gmail.com>
2023-10-29 08:02:27 +0900

    chore: Re export form component

--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -4,6 +4,7 @@ export * from './itemInfo';
 export * from './itemColumn';
 export * from './button';
 export * from './object';
+export * from './form';
 
 // export * from './button';
 // export * from './intro';
