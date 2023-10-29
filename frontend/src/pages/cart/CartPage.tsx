02bd75382a7f5aa9b3ec0f40a7ba1c2e829782e8
resetmerlin <resetmerlin@gmail.com>
2023-10-29 12:16:20 +0900

    feat: Add Child template and add useGetProducts query to fetch products

--- frontend/src/pages/cart/CartPage.tsx
+++ frontend/src/pages/cart/CartPage.tsx
@@ -1,10 +1,34 @@
-import { ParentTemplate } from '../../components/atoms';
+import {
+  AtomicTitle,
+  ChildTemplate,
+  ParentTemplate,
+} from '../../components/atoms';
 import LayoutHeader from '../../components/layouts/layoutHeader/LayoutHeader';
+import { CartAddress, OrderSummary } from '../../components/molecules';
+import { Cart } from '../../components/organisms';
+import { useGetProductsQuery } from '../../features/api/apiSlice';
 
 function CartPage() {
+  const { data } = useGetProductsQuery();
+  const cartProducts = data && [...data]?.slice(0, 3);
   return (
     <LayoutHeader>
-      <ParentTemplate size="s"></ParentTemplate>
+      <ParentTemplate size="m">
+        <ChildTemplate position="topLeft" size="m">
+          <AtomicTitle size="xs">Cart</AtomicTitle>
+        </ChildTemplate>
+        <ChildTemplate position="centerLeft" size="m">
+          <Cart cartProducts={cartProducts} />
+        </ChildTemplate>
+
+        <ChildTemplate position="right" size="m">
+          <OrderSummary />
+        </ChildTemplate>
+
+        <ChildTemplate position="bottomLeft" size="m">
+          <CartAddress />
+        </ChildTemplate>
+      </ParentTemplate>
     </LayoutHeader>
   );
 }
