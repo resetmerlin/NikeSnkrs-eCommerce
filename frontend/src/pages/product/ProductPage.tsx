96d275b9c41b32e375a78f8e9f94a5983ef51d2b
resetmerlin <resetmerlin@gmail.com>
2023-10-30 07:53:34 +0900

    feat: Add get single product api slice and put annotation

--- frontend/src/pages/product/ProductPage.tsx
+++ frontend/src/pages/product/ProductPage.tsx
@@ -1,28 +1,41 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { ChildTemplate, ParentTemplate } from '../../components/atoms';
 import { ItemInfoEvents, ItemNav } from '../../components/organisms';
-import { useGetProductsQuery } from '../../features/api/apiSlice';
+import {
+  useGetProductQuery,
+  useGetProductsQuery,
+} from '../../features/api/apiSlice';
 import LayoutHeader from '../../components/layouts/layoutHeader/LayoutHeader';
 
 export default function ProductPage() {
   const navigate = useNavigate();
   const productId = useParams();
   const { data: products } = useGetProductsQuery();
+  const { data: singleProduct } = useGetProductQuery(productId?.id);
 
+  /** 전 페이지로 이동 */
   const goPrevPage = () => navigate(-1);
 
+  /** 현재 상품 */
   const product =
     products &&
     [...products].filter((product) => product?._id == productId?.id)[0];
+
+  /** 전체 상품 속 현재 인덱스 */
   const currentIndex = products?.indexOf(product);
 
+  /** 다음 상품으로 이동 */
   const goNextProductPage = () => {
+    // 마지막 인덱스까지 도달할시
     if (currentIndex + 1 !== products.length) {
       navigate(`${products[currentIndex + 1]?._id}`);
-    } else {
+    }
+    // 마지막 인덱스가 아닐시
+    else {
       navigate(`${products[0]?._id}`);
     }
   };
+
   return (
     <LayoutHeader>
       <ParentTemplate size="full">
