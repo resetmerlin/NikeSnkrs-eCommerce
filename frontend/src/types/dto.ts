aedf230fd615c3bc3f33e582683c4d336cfe7cf2
resetmerlin <resetmerlin@gmail.com>
2023-10-30 12:43:13 +0900

    feat: Add IProducts type and IProductId interface

--- frontend/src/types/dto.ts
+++ frontend/src/types/dto.ts
@@ -1,21 +1,22 @@
 export interface IProduct {
-  product: {
-    _id: string;
-    user: string;
+  _id: string;
+  user: string;
 
-    name: string;
-    brand: string;
-    category: string;
-    description: string;
-    rating: number;
-    numReviews: number;
-    threeValue: number;
-    price: number;
+  name: string;
+  brand: string;
+  category: string;
+  description: string;
+  rating: number;
+  numReviews: number;
+  threeValue: number;
+  price: number;
 
-    countInStock: number;
-    review: string;
-    __v: number;
-    createAt: string;
-    updatedt: string;
-  };
+  countInStock: number;
+  review: string;
+  __v: number;
+  createAt: string;
+  updatedt: string;
 }
+
+export type IProducts = IProduct[];
+export interface IProductId extends Pick<IProduct, '_id'> {}
