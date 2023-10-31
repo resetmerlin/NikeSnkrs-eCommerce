c57beec73650c284746a21208d31634bd52e3341
resetmerlin <resetmerlin@gmail.com>
2023-10-31 09:22:55 +0900

    feat: Add forwardRef hook and give ref in condition

--- frontend/src/components/organisms/itemNav/ItemNav.tsx
+++ frontend/src/components/organisms/itemNav/ItemNav.tsx
@@ -1,38 +1,45 @@
+import { forwardRef } from 'react';
 import { IProduct, IProducts } from '../../../types/dto';
 import { DownButton, ItemColumn } from '../../molecules';
 import './ItemNav.scss';
+import { ItemColRef } from '../../../pages/product/ProductPage';
 
 type IProps = {
   goNextProductPage: () => void;
-  productId: string;
-  products: IProducts;
+  productId?: string;
+  products?: IProducts;
 };
 
-export default function ItemNav({
-  products,
-  productId,
-  goNextProductPage,
-}: IProps) {
+export default forwardRef<ItemColRef, IProps>(function ItemNav(
+  { products, productId, goNextProductPage },
+  ref
+) {
   return (
     <div className="itemNav">
       <div className="itemNav__column">
         {products?.map((product: IProduct) => {
-          return (
-            <ItemColumn
-              key={product?._id}
-              product={product}
-              productId={productId}
-            />
-          );
+          if (product?._id === productId) {
+            return (
+              <ItemColumn
+                ref={ref}
+                key={product?._id}
+                product={product}
+                productId={productId}
+              />
+            );
+          } else {
+            return (
+              <ItemColumn
+                key={product?._id}
+                product={product}
+                productId={productId}
+              />
+            );
+          }
         })}
       </div>
 
       <DownButton goNextProductPage={goNextProductPage} />
     </div>
   );
-}
-
-ItemNav.defaultProps = {
-  products: [],
-  productId: '',
-};
+});
