be3a704481da816c2f27651c6c43b0b9663252ac
resetmerlin <resetmerlin@gmail.com>
2023-11-01 07:23:48 +0900

    feat: Add Cart summary, fetch logic on CartPage component

--- frontend/src/pages/cart/CartPage.tsx
+++ frontend/src/pages/cart/CartPage.tsx
@@ -1,3 +1,4 @@
+import { useLocation, useParams } from 'react-router-dom';
 import {
   AtomicTitle,
   ChildTemplate,
@@ -6,11 +7,40 @@ import {
 import LayoutHeader from '../../components/layouts/layoutHeader/LayoutHeader';
 import { CartAddress, CartSummary } from '../../components/molecules';
 import { Cart } from '../../components/organisms';
-import { useGetProductsQuery } from '../../features/api/apiSlice';
+import { useAddToCartQuery } from '../../features/api/apiSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { cartDeleted } from '../../features/cart/cartReducers';
+import { IProduct } from '../../types/dto';
 
 function CartPage() {
-  const { data } = useGetProductsQuery();
-  const cartProducts = data && [...data]?.slice(0, 3);
+  const { id } = useParams();
+  const dispatch = useAppDispatch();
+  const location = useLocation().search;
+
+  const qty = Number(new URLSearchParams(location).get('qty'));
+  useAddToCartQuery({ id, qty });
+
+  const cartProducts = useAppSelector((state) => state.carts);
+  const deletOnCart = (product: IProduct) => dispatch(cartDeleted(product));
+
+  const taxPrice = 150;
+  const shippingPrice = 3000;
+  const productPrice = cartProducts
+    .reduce((acc, item) => acc + Number(item.qty) * Number(item.price), 0)
+    .toFixed(1);
+
+  const totalPrice = +productPrice - taxPrice - shippingPrice;
+
+  const paymentMethod = 'paypal';
+
+  const date = new Date();
+
+  const dateFormat = new Intl.DateTimeFormat('en-kr', {
+    dateStyle: 'full',
+  });
+
+  const currentDate = dateFormat.format(date);
+
   return (
     <LayoutHeader>
       <ParentTemplate size="m">
@@ -18,11 +48,23 @@ function CartPage() {
           <AtomicTitle size="xs">Cart</AtomicTitle>
         </ChildTemplate>
         <ChildTemplate position="centerLeft" size="m">
-          <Cart cartProducts={cartProducts} />
+          <Cart
+            cartProducts={cartProducts}
+            deletOnCart={deletOnCart}
+            qty={qty}
+          />
         </ChildTemplate>
 
         <ChildTemplate position="right" size="m">
-          <CartSummary />
+          <CartSummary
+            cartProducts={cartProducts}
+            totalPrice={totalPrice}
+            paymentMethod={paymentMethod}
+            productPrice={productPrice}
+            shippingPrice={shippingPrice}
+            taxPrice={taxPrice}
+            currentDate={currentDate}
+          />
         </ChildTemplate>
 
         <ChildTemplate position="bottomLeft" size="m">
