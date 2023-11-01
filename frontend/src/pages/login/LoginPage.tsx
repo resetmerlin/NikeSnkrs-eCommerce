4263374028a745cb6c2451360ddba6be0b0b9db0
resetmerlin <resetmerlin@gmail.com>
2023-11-01 09:31:01 +0900

    feat: Add login schema for validation and add useForm hook

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,9 +1,34 @@
+import { useForm, FormProvider } from 'react-hook-form';
 import { Layout } from '../../components/layouts/layout';
 import { ChildTemplate, ParentTemplate } from '../../components/atoms';
 import { Background, UserMemberEvents } from '../../components/organisms';
 import { LoginForm } from '../../components/molecules';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export default function LoginPage() {
+  const loginSchema = yup.object().shape({
+    userEmail: yup
+      .string()
+      .required('Please write your email')
+      .matches(/^[^\s@]+@example\.com$/, '@example.com required'),
+    userPassword: yup.string().required('Please write your password'),
+  });
+
+  const methods = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(loginSchema),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const loginSubmit = (e) => {
+    console.log(e);
+  };
   return (
     <Layout>
       <ParentTemplate size="full">
@@ -12,7 +37,14 @@ export default function LoginPage() {
         </ChildTemplate>
         <ChildTemplate size="full" position="right">
           <UserMemberEvents>
-            <LoginForm />
+            <FormProvider {...methods}>
+              <LoginForm
+                errors={errors}
+                handleSubmit={handleSubmit}
+                loginSubmit={loginSubmit}
+                register={register}
+              />
+            </FormProvider>
           </UserMemberEvents>
         </ChildTemplate>
       </ParentTemplate>
