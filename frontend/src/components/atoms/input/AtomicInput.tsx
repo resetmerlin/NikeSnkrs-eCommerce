995d5b62b03fa5092fbcd8ca3478a3bfd269105e
resetmerlin <resetmerlin@gmail.com>
2023-11-01 09:29:25 +0900

    feat: Add register props for react hook form hook

--- frontend/src/components/atoms/input/AtomicInput.tsx
+++ frontend/src/components/atoms/input/AtomicInput.tsx
@@ -3,18 +3,24 @@ import './AtomicInput.scss';
 
 interface IProps extends TNormalElementProps<HTMLInputElement> {
   type: 'password' | 'email' | 'name';
-  name?: 'userPassword' | 'userEmail' | 'userName' | 'userConfirmPassword';
+  name: 'userPassword' | 'userEmail' | 'userName' | 'userConfirmPassword';
+  register: (name: string) => {
+    ref: (instance: any) => void;
+    name: string;
+  };
 }
 
 export default function AtomicInput({
   className = '',
   name,
   type,
+  register,
   ...props
 }: IProps) {
   return (
     <input
       {...props}
+      {...register(name)}
       name={name}
       type={type}
       className={`${className} input`}
