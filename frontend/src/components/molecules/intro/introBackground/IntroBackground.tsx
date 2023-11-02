3ab18c027454d677d7fbeea7467528b8798219fe
resetmerlin <resetmerlin@gmail.com>
2023-11-02 18:07:33 +0900

    feat: Add IntroBackground molecules component

---
+++ frontend/src/components/molecules/intro/introBackground/IntroBackground.tsx
@@ -0,0 +1,11 @@
+import { AtomicTitle } from '../../../atoms';
+
+export default function IntroBackground() {
+  return (
+    <div className="intro-background">
+      <AtomicTitle size="max" color="secondary" strength="500">
+        NIKE
+      </AtomicTitle>
+    </div>
+  );
+}
