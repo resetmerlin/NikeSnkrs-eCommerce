4bf5b60c1d5e227ea83c6046e58ded5be0834ae2
resetmerlin <resetmerlin@gmail.com>
2023-11-02 13:18:25 +0900

    feat: Add delete user Info action

--- frontend/src/features/user/userReducers.ts
+++ frontend/src/features/user/userReducers.ts
@@ -1,17 +1,24 @@
 import { createSlice, current } from '@reduxjs/toolkit';
 
+const initialState = [];
+
 export const userInfoSlice = createSlice({
   name: 'userInfo',
-  initialState: [],
+  initialState: initialState,
   reducers: {
     userInfoAdded(state, action) {
       const data = action.payload;
       state.push(data);
       localStorage.setItem('userInfo', JSON.stringify(current(state)));
     },
+    userInfoDeleted: () => {
+      localStorage.removeItem('userInfo');
+
+      return initialState;
+    },
   },
 });
 
-export const { userInfoAdded } = userInfoSlice.actions;
+export const { userInfoAdded, userInfoDeleted } = userInfoSlice.actions;
 
 export default userInfoSlice.reducer;
