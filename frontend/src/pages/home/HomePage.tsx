434ed3295b88f514c599daf3f59ea0b7b39f4ce0
resetmerlin <resetmerlin@gmail.com>
2023-11-02 19:37:35 +0900

    refactor: Rename deleteUserInfo to logOut

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,5 @@
 import { ChildTemplate, ParentTemplate } from '../../components/atoms';
 import LayoutHeader from '../../components/layouts/layoutHeader/LayoutHeader';
-import {
-  IntroCenter,
-  IntroLeft,
-  IntroRight,
-} from '../../components/molecules/intro';
 import { CardLists, Intro } from '../../components/organisms';
 import CardListsSkeleton from '../../components/organisms/cardLists/CardListsSkeleton';
 import { useGetProductsQuery } from '../../features/api/apiSlice';
@@ -14,14 +9,17 @@ import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 function HomePage() {
   const dispatch = useAppDispatch();
   const { data, isLoading } = useGetProductsQuery();
+
+  /** 3 products */
   const cardProducts = data && [...data]?.slice(0, 3);
+
   const userInfo = useAppSelector((state) => state.userInfo);
 
-  const deleteUserInfo = () => {
+  const logOut = () => {
     dispatch(userInfoDeleted());
   };
   return (
-    <LayoutHeader userInfo={userInfo} deleteUserInfo={deleteUserInfo}>
+    <LayoutHeader userInfo={userInfo} logOut={logOut}>
       <ParentTemplate size="s">
         <ChildTemplate position="center" size="s">
           <Intro />
