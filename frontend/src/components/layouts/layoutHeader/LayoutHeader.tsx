fcea597c82071cae0fc813e243511e778a97be05
resetmerlin <resetmerlin@gmail.com>
2023-11-02 13:23:55 +0900

    feat: Add userInfo, deleteUserInfo props in LayoutHeader component

--- frontend/src/components/layouts/layoutHeader/LayoutHeader.tsx
+++ frontend/src/components/layouts/layoutHeader/LayoutHeader.tsx
@@ -1,10 +1,14 @@
 import { PropsWithChildren } from 'react';
 import { Header } from '../../organisms';
 
-export default function LayoutHeader({ children }: PropsWithChildren) {
+export default function LayoutHeader({
+  children,
+  userInfo,
+  deleteUserInfo,
+}: PropsWithChildren) {
   return (
     <>
-      <Header />
+      <Header userInfo={userInfo} deleteUserInfo={deleteUserInfo} />
       <main id="container">{children}</main>
     </>
   );
