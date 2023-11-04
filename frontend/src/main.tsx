d1a24925691688e675077be686ed7aa672461df3
resetmerlin <resetmerlin@gmail.com>
2023-11-04 11:16:19 +0900

    feat: Add PersistGate on Main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
-import { store } from './store.ts';
+import { persistor, store } from './store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
