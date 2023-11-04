9d6d92dd047d6ffd53127f03272f94e0b7b9ab55
resetmerlin <resetmerlin@gmail.com>
2023-11-04 09:29:34 +0900

    feat: Add api call after form submit

--- frontend/src/pages/profile/ProfilePage.tsx
+++ frontend/src/pages/profile/ProfilePage.tsx
@@ -2,11 +2,12 @@ import LayoutHeader from '../../components/layouts/layoutHeader/LayoutHeader';
 import { ChildTemplate, ParentTemplate } from '../../components/atoms';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { selectUser } from '../../features/user/userInfoSlice';
-import { logOut } from '../../hooks';
+import { localUserToState, logOut } from '../../hooks';
 import { UserAddress, UserInfo } from '../../components/organisms';
 import { FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { registerSchema } from '../../components/schema';
+import { useUserChangedMutation } from '../../features/api/apiSlice';
 
 export type ProfileData = {
   userEmail: string;
@@ -21,23 +22,32 @@ export default function ProfilePage() {
     logOut(dispatch);
   };
 
+  const [userChange, { error, data }] = useUserChangedMutation();
+
   const methods = useForm({
     mode: 'onChange',
     resolver: yupResolver(registerSchema),
   });
 
   const profileSubmit = (data: ProfileData) => {
-    const user = {
-      name: data.userName,
-      email: data.userEmail,
-      password: data.userPassword,
-    };
+    if (userInfo[0]._id) {
+      const user = {
+        _id: userInfo[0]._id,
+        name: data.userName,
+        email: data.userEmail,
+        password: data.userPassword,
+        token: userInfo[0].token,
+      };
+      userChange(user);
+    }
   };
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = methods;
+  localUserToState(userInfo, dispatch);
 
   return (
     <LayoutHeader userInfo={userInfo} logOut={logOutHandler}>
@@ -45,6 +55,8 @@ export default function ProfilePage() {
         <ChildTemplate position="centerLeft" size="s">
           <FormProvider>
             <UserInfo
+              data={data}
+              error={error}
               errors={errors}
               register={register}
               handleSubmit={handleSubmit}
