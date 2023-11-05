05157c39c76e91418e0e60eca935ad377f632c89
resetmerlin <resetmerlin@gmail.com>
2023-11-05 10:45:28 +0900

    feat: Delete unecessary html element

--- frontend/src/components/molecules/cartAddress/CartAddress.tsx
+++ frontend/src/components/molecules/cartAddress/CartAddress.tsx
@@ -1,23 +1,18 @@
+import { Link } from 'react-router-dom';
 import { AtomicButton, AtomicSubtitle, AtomicTitle } from '../../atoms';
 import './CartAddress.scss';
 
-export default function CartAddress() {
+export default function CartAddress({ address }) {
   return (
     <div className="cardAddress">
       <AtomicTitle size="xs">Address</AtomicTitle>
+
       <div>
-        <AtomicSubtitle size="m">Postal Address:</AtomicSubtitle>
+        <AtomicSubtitle size="m">Address: {address?.address}</AtomicSubtitle>
       </div>
-      <div>
-        <AtomicSubtitle size="m">Address:</AtomicSubtitle>
-      </div>
-      <div>
-        <AtomicSubtitle size="m">Specific Address:</AtomicSubtitle>
-      </div>
-      <div>
-        <AtomicSubtitle size="m">Referenc Item:</AtomicSubtitle>
-      </div>
-      <AtomicButton size="m">Change Address</AtomicButton>
+      <Link to="/profile">
+        <AtomicButton size="m">Change Address</AtomicButton>{' '}
+      </Link>
     </div>
   );
 }
