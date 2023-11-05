07c65a7b50593500ce0c194532de4bc31a025de6
resetmerlin <resetmerlin@gmail.com>
2023-11-05 12:40:44 +0900

    feat: Add order page route on router

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -6,6 +6,7 @@ import LoginPage from '../pages/login/LoginPage';
 import RegisterPage from '../pages/register/RegisterPage';
 import CartPage from '../pages/cart/CartPage';
 import ProfilePage from '../pages/profile/ProfilePage';
+import OrderPage from '../pages/order/OrderPage';
 
 const router = createBrowserRouter([
   {
@@ -29,6 +30,7 @@ const router = createBrowserRouter([
     ],
   },
   { path: '/profile', element: <ProfilePage /> },
+  { path: '/order', element: <OrderPage /> },
 ]);
 
 export default router;
