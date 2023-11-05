bb87f2967eed4fc3ace50771ba1add138f952aba
resetmerlin <resetmerlin@gmail.com>
2023-11-05 10:46:49 +0900

    feat: Add props to handle  form submit

--- frontend/src/components/organisms/userAddress/UserAddress.tsx
+++ frontend/src/components/organisms/userAddress/UserAddress.tsx
@@ -6,23 +6,35 @@ import {
 } from '../../atoms';
 import './UserAddress.scss';
 
-export default function UserAddress() {
+export default function UserAddress({
+  addressHandler,
+  register,
+  handleSubmit,
+  addressSubmit,
+  addressInfo,
+}) {
   return (
-    <form className="userAddress">
+    <form className="userAddress" onSubmit={handleSubmit(addressSubmit)}>
       <div>
         <AtomicTitle size="xs">User address:</AtomicTitle>
+        <AtomicButton
+          type="button"
+          size="m"
+          shape="normal"
+          color="secondary"
+          onClick={addressHandler}
+        >
+          Get Location
+        </AtomicButton>
       </div>
-      <AtomicLabel htmlFor="postalCode">Postal Code</AtomicLabel>
-      <AtomicInput type="name" name="postalCode" />
 
       <AtomicLabel htmlFor="address">Address</AtomicLabel>
-      <AtomicInput type="name" name="address" />
-
-      <AtomicLabel htmlFor="specificAddress">Enter password</AtomicLabel>
-      <AtomicInput type="name" name="specificAddress" />
-
-      <AtomicLabel htmlFor="referenceItem">Confirm password</AtomicLabel>
-      <AtomicInput type="name" name="referenceItem" />
+      <AtomicInput
+        type="name"
+        name="address"
+        register={register}
+        placeholder={addressInfo?.address}
+      />
 
       <AtomicButton type="submit" size="m" shape="normal">
         Update Address
