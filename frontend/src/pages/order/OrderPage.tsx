68013ae579d76932f905ac5abeca5b5bff0d0605
resetmerlin <resetmerlin@gmail.com>
2023-11-05 16:13:51 +0900

    feat: Add hooks to fetch paypal client id

--- frontend/src/pages/order/OrderPage.tsx
+++ frontend/src/pages/order/OrderPage.tsx
@@ -3,27 +3,67 @@ import { ChildTemplate, ParentTemplate } from '../../components/atoms';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { logOut } from '../../hooks';
 import { selectUser } from '../../features/user/userInfoSlice';
-import { Cart } from '../../components/organisms';
-import { ICart, ICarts } from '../../types/dto';
-import { cartDeleted, selectCart } from '../../features/cart/cartSlice';
+import { Cart, OrderInfo } from '../../components/organisms';
+import { IOrder } from '../../types/dto';
+import { selectOrder } from '../../features/order/orderSlice';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function OrderPage() {
   const dispatch = useAppDispatch();
   const userInfo = useAppSelector(selectUser);
-  const cart: ICarts = useAppSelector(selectCart);
-  /** Delete product on cart */
-  const deletOnCart = (product: ICart['product']) =>
-    dispatch(cartDeleted(product));
+  const order: IOrder = useAppSelector(selectOrder);
+  const { id: orderId } = useParams();
 
+  const [clientId, setClientId] = useState(null);
+  const [paypalPaid, setPaypalPaid] = useState(false);
+
+  const checkPaypal = (paid) => {
+    if (paid) {
+      setPaypalPaid(true);
+    }
+  };
   const logOutHandler = () => {
     logOut(dispatch);
   };
 
+  useEffect(() => {
+    const addPayPalScript = async () => {
+      try {
+        const response = await fetch(`/api/config/paypal`);
+        if (!response.ok) {
+          throw new Error(`Request failed with status: ${response.status}`);
+        }
+        const clientId = await response.text();
+        setClientId(clientId);
+      } catch (error) {
+        console.error('Error fetching PayPal client ID:', error);
+      }
+    };
+    if (paypalPaid == false) {
+      addPayPalScript();
+    }
+  }, [order, orderId, paypalPaid]);
+
+  const date = new Date();
+  const dateHandler = new Intl.DateTimeFormat('en-kr', {
+    dateStyle: 'full',
+  });
+
+  const currentDate = dateHandler.format(date);
+
   return (
     <LayoutHeader userInfo={userInfo} logOut={logOutHandler}>
       <ParentTemplate size="s">
-        <ChildTemplate position="centerLeft" size="m">
-          <Cart cartProducts={cart} deletOnCart={deletOnCart} />
+        <ChildTemplate position="center" size="full">
+          <OrderInfo
+            paypalPaid={paypalPaid}
+            order={order}
+            checkPaypal={checkPaypal}
+            clientId={clientId}
+            date={date}
+            currentDate={currentDate}
+          />
         </ChildTemplate>
       </ParentTemplate>
     </LayoutHeader>
