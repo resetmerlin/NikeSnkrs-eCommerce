a118df7b4991c24be210af7044e9291e6054f31a
resetmerlin <resetmerlin@gmail.com>
2023-11-13 10:00:56 +0900

    refactor: Merge logic that includes navigate hook

--- frontend/src/pages/product/ProductPageHook.ts
+++ frontend/src/pages/product/ProductPageHook.ts
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { NavigateFunction, useNavigate } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { ItemColRef } from './ProductPage';
 import { selectUser, useGetProductsQuery } from '../../features';
 import { IProduct, IUser } from '../../types';
 import { logOut, useAppDispatch, useAppSelector } from '../../hooks';
 
 /** Product Observing hook */
-export const useProductObserver = (
+const useProductObserver = (
   columnRef: React.RefObject<ItemColRef>,
   goNextProductPage: () => void
 ) => {
@@ -38,7 +38,7 @@ export const useProductObserver = (
 };
 
 /** Get Products related hook */
-export const useFetchProducts = (
+const useFetchProducts = (
   productId: string | undefined
 ): [IProduct | undefined, IProduct[] | undefined, number] => {
   const { data: products } = useGetProductsQuery();
@@ -56,54 +56,67 @@ export const useFetchProducts = (
   return [product, products, currentIndex];
 };
 
-/** Go next product page hook */
-export const useGoNextPage = (
-  navigate: NavigateFunction,
-  currentIndex: number,
-  products: IProduct[] | undefined
-): void => {
-  if (currentIndex + 1 !== products?.length && products) {
-    navigate(`${products[currentIndex + 1]?._id}`);
-  }
-  // Go first if reaches last index
-  else if (products) {
-    navigate(`${products[0]?._id}`);
-  }
-};
-
-/** go cart page with qty hook */
-export const useAddToCart = (
-  e: React.FormEvent<HTMLFormElement>,
-  navigate: NavigateFunction,
-  product: IProduct | undefined
-) => {
-  e.preventDefault();
-  const qty = e.currentTarget?.productSelect.value;
-
-  if (product?.countInStock === 0) {
-    alert('Out of Stock');
-  } else {
-    navigate(`/cart/${product?._id}?qty=${qty}`);
-  }
-};
-
-/** User action hook */
-export const useUserActionHandler = (): [
-  userInfo: IUser,
+export const useProductPage = (): [
+  addToCart: (e: React.FormEvent<HTMLFormElement>) => void,
+  goNextProductPage: () => void,
+  columnRef: React.RefObject<ItemColRef>,
   logOutHandler: () => void,
   goPrevPage: () => void,
+  userInfo: IUser,
+  product: IProduct | undefined,
+  products: IProduct[] | undefined,
+  productId: string | undefined,
 ] => {
-  const userInfo: IUser = useAppSelector(selectUser);
-  const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { id: productId } = useParams();
+  const columnRef = useRef<ItemColRef | null>(null);
 
-  const logOutHandler = () => {
-    logOut(dispatch);
-  };
+  const userInfo: IUser = useAppSelector(selectUser);
+  const [product, products, currentIndex] = useFetchProducts(productId);
 
   const goPrevPage = () => {
     navigate(-1);
   };
 
-  return [userInfo, logOutHandler, goPrevPage];
+  const logOutHandler = () => {
+    logOut(dispatch);
+  };
+
+  /** Go next product page */
+  const goNextProductPage = () => {
+    if (currentIndex + 1 !== products?.length && products) {
+      navigate(`${products[currentIndex + 1]?._id}`);
+    }
+    // Go first if reaches last index
+    else if (products) {
+      navigate(`${products[0]?._id}`);
+    }
+  };
+  /** go cart page with quantity */
+  const addToCart = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const qty = e.currentTarget?.productSelect.value;
+
+    if (product?.countInStock === 0) {
+      alert('Out of Stock');
+    } else {
+      navigate(`/cart/${product?._id}?qty=${qty}`);
+    }
+  };
+
+  /** Observer Product */
+  useProductObserver(columnRef, goNextProductPage);
+
+  return [
+    addToCart,
+    goNextProductPage,
+    columnRef,
+    logOutHandler,
+    goPrevPage,
+    userInfo,
+    product,
+    products,
+    productId,
+  ];
 };
