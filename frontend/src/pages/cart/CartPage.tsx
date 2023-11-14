49202c63b3cdd63143f8164545c644882cd09cf7
resetmerlin <resetmerlin@gmail.com>
2023-11-14 09:39:04 +0900

    refactor: Split business logic using hook in CartPage

--- frontend/src/pages/cart/CartPage.tsx
+++ frontend/src/pages/cart/CartPage.tsx
@@ -1,5 +1,3 @@
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import { useEffect } from 'react';
 import {
   AtomicTitle,
   Cart,
@@ -9,90 +7,24 @@ import {
   HeaderLayout,
   ParentTemplate,
 } from '../../components';
-import {
-  cartDeleted,
-  selectAddress,
-  selectCart,
-  selectUser,
-  useAddToCartMutation,
-  useAddToOrderMutation,
-} from '../../features';
-import { goToLogin, logOut, useAppDispatch, useAppSelector } from '../../hooks';
-import { IAddress, ICart, ICarts, IUser } from '../../types';
+import { useCartPage } from './CartPage.hook';
 
 function CartPage() {
-  const { id } = useParams();
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const location = useLocation().search;
-
-  const qty = Number(new URLSearchParams(location).get('qty'));
-
-  /** fetch chosen product */
-  const [addToCart] = useAddToCartMutation();
-  const [addToOrder, { data: orderData }] = useAddToOrderMutation();
-
-  const carts: ICarts = useAppSelector(selectCart);
-  const userInfo: IUser = useAppSelector(selectUser);
-  const address: IAddress = useAppSelector(selectAddress);
-
-  /** Check user auth, if no auth go to login page */
-  goToLogin(userInfo, navigate);
-
-  /** Delete product on cart */
-  const deletOnCart = (product: ICart['product']) =>
-    dispatch(cartDeleted(product));
-
-  const logOutHandler = () => {
-    logOut(dispatch);
-  };
-
-  // Numbers for the payment summary
-  const taxPrice = 15;
-  const shippingPrice = 3;
-  const productPrice = carts
-    .reduce((acc, item) => acc + Number(item?.qty) * Number(item?.price), 0)
-    .toFixed(1);
-  const date = new Date();
-  const dateFormat = new Intl.DateTimeFormat('en-kr', {
-    dateStyle: 'full',
-  });
-  const paymentMethod = 'paypal';
-  const totalPrice = +productPrice - taxPrice - shippingPrice;
-
-  // Add to Order
-  const addToOrderHandler = () => {
-    const order = {
-      email: userInfo?.email,
-      name: userInfo?.name,
-      carts,
-      shippingAddress: {
-        address: address?.address,
-      },
-      paymentMethod,
-      itemsPrice: productPrice,
-      taxPrice,
-      shippingPrice,
-      totalPrice,
-      token: userInfo?.token,
-    };
-
-    addToOrder(order);
-  };
-
-  /** If Ordered, go order page */
-  useEffect(() => {
-    if (orderData) {
-      navigate(`/order/${orderData._id}`);
-    }
-  }, [orderData]);
-
-  /** Fetch Product if id, qty exists */
-  useEffect(() => {
-    if (id && qty) {
-      addToCart({ id, qty });
-    }
-  }, [id, qty]);
+  const [
+    userInfo,
+    logOutHandler,
+    carts,
+    deletOnCart,
+    taxPrice,
+    shippingPrice,
+    productPrice,
+    totalPrice,
+    paymentMethod,
+    date,
+    dateFormat,
+    addToOrderHandler,
+    address,
+  ] = useCartPage();
 
   return (
     <HeaderLayout userInfo={userInfo} logOut={logOutHandler}>
